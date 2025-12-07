d723581ff34b91fa7e900f075a2ba447dc623c39
Pascal Delange <pascal@checkmarble.com>
2025-12-07 10:25:54 +0100

    first working example

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	computeStats()
+}
+
+type result struct {
+	count int
+	sum   int
+	max   int
+	min   int
+}
+
+func computeStats() {
+	fileName := flag.String("filename", "./data/measurements_1M.txt", "file to read")
+	flag.Parse()
+	fmt.Println("reading file " + *fileName)
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	resultsMap := make(map[string]result, 10_000)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatalln(err)
+		}
+		text := scanner.Text()
+		textSl := strings.Split(text, ";")
+		textSl[1] = strings.Replace(textSl[1], ".", "", 1)
+		val, _ := strconv.ParseInt(textSl[1], 0, 64)
+		valI := int(val)
+		if _, ok := resultsMap[textSl[0]]; ok {
+			resultsMap[textSl[0]] = result{
+				count: resultsMap[textSl[0]].count + 1,
+				sum:   resultsMap[textSl[0]].sum + valI,
+				min:   min(resultsMap[textSl[0]].min, valI),
+				max:   max(resultsMap[textSl[0]].max, valI),
+			}
+		} else {
+			resultsMap[textSl[0]] = result{
+				count: 1,
+				sum:   valI,
+				min:   valI,
+				max:   valI,
+			}
+		}
+	}
+}
